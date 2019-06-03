04909148824d094ba1489eae428f2d03cb704c5e
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2019-06-03 10:30:11 +0200

    Add headerbar component

--- frontend/zes-startlijst/src/app/app.module.ts
+++ frontend/zes-startlijst/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent
   ],
   imports: [
     BrowserModule
