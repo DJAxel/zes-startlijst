a82ddd3d2aa65452d71a98cc227b9fa19835c143
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2019-06-08 15:12:00 +0200

    Add add-flight component and create the first two routes

--- frontend/zes-startlijst/src/app/app.module.ts
+++ frontend/zes-startlijst/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderbarComponent } from './headerbar/headerbar.component';
 import { StartlistComponent } from './startlist/startlist.component';
+import { AddFlightComponent } from './add-flight/add-flight.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderbarComponent,
-    StartlistComponent
+    StartlistComponent,
+    AddFlightComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
