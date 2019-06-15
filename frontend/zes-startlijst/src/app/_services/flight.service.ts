3476da41cd1cdd0f62f8313fdf46209948b2b88f
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2019-06-16 00:46:44 +0200

    Remove hardcoded Flights data

--- frontend/zes-startlijst/src/app/_services/flight.service.ts
+++ frontend/zes-startlijst/src/app/_services/flight.service.ts
@@ -14,8 +14,6 @@ import { map } from 'rxjs/operators';
 })
 export class FlightService {
 
-  private flights: Flight[] = [];
-
   getAll(): Observable<Flight[]> {
     return this.http.get<Flight[]>('/api/flights/').pipe(
       map(
@@ -24,54 +22,5 @@ export class FlightService {
     );
   }
 
-  constructor(private http: HttpClient) {
-    this.flights.push(
-      new Flight(
-        new Plane("PH-421", "Ka8", 1),
-        new Pilot("Axel Köhler", Pilotstatus.Solist),
-        null,
-        new Date("2019-06-03 10:33:00"),
-        new Date("2019-06-03 10:45:00"),
-        "EHDP",
-        Startmethod.L,
-        ""
-      )
-    );
-    this.flights.push(
-      new Flight(
-        new Plane("PH-1606", "Ka8", 1),
-        new Pilot("Stefan Brilmans", Pilotstatus.DBO),
-        new Pilot("Abraham Veldhuis", Pilotstatus.Instructeur),
-        new Date("2019-06-03 10:40:00"),
-        new Date("2019-06-03 10:59:00"),
-        "EHDP",
-        Startmethod.L,
-        ""
-      )
-    );
-    this.flights.push(
-      new Flight(
-        new Plane("PH-1606", "Ka8", 1),
-        new Pilot("Stefan Brilmans", Pilotstatus.DBO),
-        new Pilot("Abraham Veldhuis", Pilotstatus.Instructeur),
-        new Date("2019-06-03 10:40:00"),
-        null,
-        "EHDP",
-        Startmethod.L,
-        ""
-      )
-    );
-    this.flights.push(
-      new Flight(
-        new Plane("PH-1016", "Ka8", 1),
-        new Pilot("Axel Köhler", Pilotstatus.Solist),
-        null,
-        new Date("2019-06-03 11:06:00"),
-        new Date("2019-06-03 11:07:00"),
-        "EHDP",
-        Startmethod.L,
-        ""
-      )
-    );
-  }
+  constructor(private http: HttpClient) { }
 }
