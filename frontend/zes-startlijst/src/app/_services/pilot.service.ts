88cb9851b3ae5155c35388bab3d86787c7e0a281
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2019-06-15 13:11:34 +0200

    Retrieve planes from API via http aswell, cleanup any hardcoded data setup

--- frontend/zes-startlijst/src/app/_services/pilot.service.ts
+++ frontend/zes-startlijst/src/app/_services/pilot.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Pilot } from '../_domain/pilot';
 import { Pilotstatus } from '../_domain/pilotstatus';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
@@ -12,16 +12,9 @@ export class PilotService {
 
   private pilots: Pilot[] = [];
 
-  constructor(private http: HttpClient) {
-    this.pilots.push(
-      new Pilot("Axel Köhler", Pilotstatus.Solist),
-      new Pilot("Abraham Veldhuis", Pilotstatus.Instructeur),
-      new Pilot("Stefan Brilmans", Pilotstatus.DBO),
-    );
-  }
+  constructor(private http: HttpClient) { }
 
   getAll(): Observable<Pilot[]> {
-    // return this.http.get<Pilot[]>(this.url);
     return this.http.get<Pilot[]>('/api/pilots');
   }
 }
--- frontend/zes-startlijst/src/app/_services/plane.service.ts
+++ frontend/zes-startlijst/src/app/_services/plane.service.ts
@@ -1,27 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Plane } from '../_domain/plane';
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaneService {
 
-  private planes: Plane[] = [];
-
   getAll(): Observable<Plane[]> {
-    return of(this.planes);
+    return this.http.get<Plane[]>('/api/planes');
   }
 
-  constructor() {
-    this.planes.push(
-      new Plane("PH-1016", "LS-4b", 1),
-      new Plane("PH-1134", "Twin III Acro", 2),
-      new Plane("PH-1259", "HPH 304C", 1),
-      new Plane("PH-421", "Ka8", 1),
-      new Plane("PH-1606", "ASK21", 2),
-      new Plane("PH-980", "SZD Junior", 1)
-    );
-  }
+  constructor(private http: HttpClient) { }
 }
