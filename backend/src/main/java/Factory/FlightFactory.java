1726f43e9fd369fa33da17a52736af04565fe5df
Köhler,Axel A.T <a.kohler@student.fontys.nl>
2019-06-17 13:37:38 +0200

    Use all testcontexts for deployed demo

--- backend/src/main/java/Factory/FlightFactory.java
+++ backend/src/main/java/Factory/FlightFactory.java
@@ -1,11 +1,12 @@
 package Factory;
 
 import DAL.Contexts.jpa.FlightJpaContext;
+import DAL.Contexts.test.FlightTestContext;
 import DAL.Repositories.FlightRepository;
 import DAL_interfaces.Repositories.IFlightRepository;
 
 public class FlightFactory {
     public static IFlightRepository getFlightRepository() {
-        return new FlightRepository( new FlightJpaContext() );
+        return new FlightRepository( new FlightTestContext() );
     }
 }
--- backend/src/main/java/Factory/UserFactory.java
+++ backend/src/main/java/Factory/UserFactory.java
@@ -1,11 +1,12 @@
 package Factory;
 
 import DAL.Contexts.jpa.UserJpaContext;
+import DAL.Contexts.test.UserTestContext;
 import DAL.Repositories.UserRepository;
 import DAL_interfaces.Repositories.IUserRepository;
 
 public class UserFactory {
     public static IUserRepository getUserRepository(){
-        return new UserRepository( new UserJpaContext() );
+        return new UserRepository( new UserTestContext() );
     };
 }
